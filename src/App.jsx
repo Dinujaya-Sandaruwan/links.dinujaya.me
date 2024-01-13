953260455ff590c381baec9398e58a67dc062026
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-01-13 09:32:35 +0530

    Build: short URL input area

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ function App() {
   return (
     <main>
       <h1 className="main-title">Let's Create Short Links</h1>
+      <section className="short-url-area">
+        <h2 className="paste-here-title">Paste the URL to be shortened</h2>
+        <div className="input-area">
+          <input type="text" className="url-input" />
+          <button>Short Now</button>
+        </div>
+        <p className="info-text">
+          short.dinujaya.com, crafted by Dinujaya, serves as a complimentary
+          utility designed for URL abbreviation and the generation of succinct
+          links.
+        </p>
+      </section>
     </main>
   );
 }
